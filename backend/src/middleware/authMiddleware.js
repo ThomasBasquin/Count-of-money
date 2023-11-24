85dc4196026c71b601d69d131163ea329ed11adb
Thomas <thomas.basquin2@gmail.com>
2023-11-24 23:30:08 +0100

    Fix session user check in authMiddleware.js

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,12 +1,12 @@
 const isAuth = (req, res, next) => {
-  if (req.session.userId) {
+  if (req.session.user) {
     return next();
   }
   res.status(401).json({ message: 'Non autorisé' });
 };
 
 const isNotAuth = (req, res, next) => {
-  if (!req.session.userId) {
+  if (!req.session.user) {
     return next();
   }
   res.status(400).json({ message: 'Déjà connecté' });
