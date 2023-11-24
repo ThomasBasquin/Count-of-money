b6e2c93e75b03dedc59d84c42c7030f1c8e42866
Thomas <thomas.basquin2@gmail.com>
2023-11-24 23:30:52 +0100

    Add userRoutes and error with code

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -3,48 +3,76 @@ import userService from '../services/userService.js';
 const userController = {
   register: async (req, res) => {
     try {
-      const user = await userService.Create(req.body);
-      res.status(201).json(user);
+      const user = await userService.create(req.body);
+      res
+        .status(201)
+        .json({ id: user._id, username: user.username, email: user.email });
     } catch (error) {
+      if (error.message.startsWith('Un utilisateur existe déjà')) {
+        return res.status(409).json({ message: error.message });
+      }
+      if (error.message.startsWith('Le mot de passe')) {
+        return res.status(400).json({ message: error.message });
+      }
+      if (error.message.startsWith("Le nom d'utilisateur")) {
+        return res.status(400).json({ message: error.message });
+      }
       res.status(500).json({ message: error.message });
     }
   },
   login: async (req, res) => {
     try {
-      const user = await userService.Login(req.body);
+      const user = await userService.login(req.body);
       req.session.user = user;
-      res.status(200).json(user);
+      res
+        .status(200)
+        .json({ id: user._id, username: user.username, email: user.email });
     } catch (error) {
+      if (error.message.startsWith('Aucun utilisateur trouvé')) {
+        return res.status(404).json({ message: error.message });
+      }
+      if (error.message.startsWith('Mot de passe incorrect')) {
+        return res.status(401).json({ message: error.message });
+      }
       res.status(500).json({ message: error.message });
     }
   },
   getProfile: async (req, res) => {
     try {
-      const user = await userService.GetProfile(req.session.user._id);
-      res.status(200).json(user);
+      const user = await userService.getProfile(req.session.user._id);
+      res
+        .status(200)
+        .json({ id: user._id, username: user.username, email: user.email });
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
   },
   updateProfile: async (req, res) => {
     try {
-      const user = await userService.UpdateProfile(
-        req.session.user._id,
+      const user = await userService.updateProfile(
+        req.session.user.id,
         req.body
       );
-      res.status(200).json(user);
+      res
+        .status(200)
+        .json({ id: user._id, username: user.username, email: user.email });
     } catch (error) {
+      if (error.message.startsWith('Aucun utilisateur trouvé')) {
+        return res.status(404).json({ message: error.message });
+      }
+      if (error.message.startsWith("Le nom d'utilisateur")) {
+        return res.status(400).json({ message: error.message });
+      }
       res.status(500).json({ message: error.message });
     }
   },
   logout: async (req, res) => {
-    try {
-      await userService.logout(req.session.user._id);
+    if (req.session.user) {
       req.session.destroy();
-      res.status(200).json({ message: 'User logged out' });
-    } catch (error) {
-      res.status(500).json({ message: error.message });
+      res.clearCookie('connect.sid');
+      return res.status(200).json({ message: 'Déconnexion réussie' });
     }
+    res.status(401).json({ message: 'Non autorisé' });
   },
 };
 
--- backend/src/services/userService.js
+++ backend/src/services/userService.js
@@ -2,13 +2,42 @@ import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
 
 const userService = {
-  Create: async ({ username, email, password }) => {
+  create: async ({ username, email, password }) => {
+    const sameEmail = await User.findOne({ email });
+    const sameUsername = await User.findOne({ username });
+
+    if (sameEmail || sameUsername) {
+      throw new Error('Un utilisateur existe déjà avec ces informations.');
+    }
+
+    if (password.length < 6) {
+      throw new Error('Le mot de passe doit contenir au moins 6 caractères.');
+    }
+    if (password.includes(' ')) {
+      throw new Error("Le mot de passe ne doit pas contenir d'espace.");
+    }
+    if (username.length < 3) {
+      throw new Error(
+        "Le nom d'utilisateur doit contenir au moins 3 caractères."
+      );
+    }
+    if (username.length > 15) {
+      throw new Error(
+        "Le nom d'utilisateur doit contenir au maximum 15 caractères."
+      );
+    }
+    if (username.includes(' ')) {
+      throw new Error("Le nom d'utilisateur ne doit pas contenir d'espace.");
+    }
+
     const user = new User({ username, email, password });
     await user.save();
     return user;
   },
-  Login: async ({ email, password }) => {
+
+  login: async ({ email, password }) => {
     const user = await User.findOne({ email });
+    console.log(user);
     if (!user) {
       throw new Error('Aucun utilisateur trouvé avec cet email.');
     }
@@ -18,6 +47,49 @@ const userService = {
       throw new Error('Mot de passe incorrect.');
     }
 
+    return user;
+  },
+
+  getProfile: async userId => {
+    const user = await User.findById(userId);
+    if (!user) {
+      throw new Error('Aucun utilisateur trouvé.');
+    }
+    return user;
+  },
+
+  updateProfile: async (userId, updateData) => {
+    const user = await User.findById(userId);
+    if (!user) {
+      throw new Error('Aucun utilisateur trouvé.');
+    }
+
+    if (updateData.username) {
+      if (updateData.username.length < 3) {
+        throw new Error(
+          "Le nom d'utilisateur doit contenir au moins 3 caractères."
+        );
+      }
+      if (updateData.username.length > 15) {
+        throw new Error(
+          "Le nom d'utilisateur doit contenir au maximum 15 caractères."
+        );
+      }
+      if (updateData.username.includes(' ')) {
+        throw new Error("Le nom d'utilisateur ne doit pas contenir d'espace.");
+      }
+      user.username = updateData.username;
+    }
+    if (updateData.defaultCurrency) {
+      if (!['USD', 'EUR', 'GBP', 'JPY'].includes(updateData.defaultCurrency)) {
+        throw new Error('La devise par défaut est invalide.');
+      }
+      user.defaultCurrency = updateData.defaultCurrency;
+    }
+    if (updateData.pressReviewKeywords)
+      user.pressReviewKeywords = updateData.pressReviewKeywords;
+
+    await user.save();
     return user;
   },
 };
