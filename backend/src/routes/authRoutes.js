b6e84de23170bc178b44cee0de341a011f632360
Pirooooooo <pierre.halm@epitech.eu>
2023-11-27 16:13:38 +0100

    Add swagger for auth and crypto routes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -4,11 +4,56 @@ import passport from 'passport';
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /google:
+ *   get:
+ *     summary: Google Authentication
+ *     description: Redirects the user to the Google authentication screen.
+ *     tags:
+ *       - Authentication
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       302:
+ *         description: Redirect to the Google authentication screen.
+ *       401:
+ *         description: User is not authenticated.
+ *     deprecated: false
+ */
+
 router.get(
   '/google',
   isNotAuth,
   passport.authenticate('google', { scope: ['profile', 'email'] })
 );
+
+/**
+ * @swagger
+ * /google/callback:
+ *   get:
+ *     summary: Google Authentication Callback
+ *     description: Handles the callback from Google authentication after the user has granted access.
+ *     tags:
+ *       - Authentication
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - name: code
+ *         in: query
+ *         description: Authorization code returned by Google after user authorization.
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       302:
+ *         description: Redirect to the home page after successful authentication.
+ *       401:
+ *         description: Authentication failure, redirect to the login page.
+ *       500:
+ *         description: Internal server error.
+ *     deprecated: false
+ */
 
 router.get(
   '/google/callback',
--- backend/src/routes/cryptoRoutes.js
+++ backend/src/routes/cryptoRoutes.js
@@ -60,17 +60,119 @@ const router = express.Router();
 
 router.get('/cryptos', cryptoController.getList);
 
-// GET les informations détaillées d'une crypto-monnaie spécifique
+/**
+ * @swagger
+ * /cryptos/{cmid}:
+ *   get:
+ *     summary: Get Details of a Specific Cryptocurrency
+ *     description: Retrieves details of a cryptocurrency based on its ID.
+ *     tags:
+ *       - Cryptos
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - name: cmid
+ *         in: path
+ *         description: Cryptocurrency ID.
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Successful retrieval of cryptocurrency details.
+ *       401:
+ *         description: User is not authenticated.
+ *       404:
+ *         description: The specified cryptocurrency was not found.
+ *       500:
+ *         description: Internal server error.
+ *     deprecated: false
+ */
+
 router.get('/cryptos/:cmid', isAuth, cryptoController.getDetails);
 
-// GET l'historique des prix d'une crypto-monnaie pour une période donnée
+
+/**
+ * @swagger
+ * /cryptos/{cmid}/history/{period}:
+ *   get:
+ *     summary: Get Price History of a Specific Cryptocurrency
+ *     description: Retrieves the price history of a specific cryptocurrency based on its ID and a specified period.
+ *     tags:
+ *       - Cryptos
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - name: cmid
+ *         in: path
+ *         description: Cryptocurrency ID.
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - name: period
+ *         in: path
+ *         description: Time period for the price history (e.g., 'daily', 'weekly', 'monthly').
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Successful retrieval of cryptocurrency price history.
+ *       401:
+ *         description: User is not authenticated.
+ *       404:
+ *         description: The specified cryptocurrency or period was not found.
+ *       500:
+ *         description: Internal server error.
+ *     deprecated: false
+ */
+
 router.get(
   '/cryptos/:cmid/history/:period',
   isAuth,
   cryptoController.getPriceHistory
 );
 
-// POST pour ajouter une nouvelle crypto-monnaie (seulement admin)
+
+/**
+ * @swagger
+ * /cryptos:
+ *   post:
+ *     summary: Add a New Cryptocurrency
+ *     description: Adds a new cryptocurrency to the system.
+ *     tags:
+ *       - Cryptos
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       description: Data for the new cryptocurrency.
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: The name of the cryptocurrency.
+ *               symbol:
+ *                 type: string
+ *                 description: The symbol of the cryptocurrency.
+ *               // Add other required properties for the cryptocurrency here.
+ *     responses:
+ *       201:
+ *         description: Cryptocurrency added successfully.
+ *       401:
+ *         description: User is not authenticated.
+ *       403:
+ *         description: User does not have the necessary permissions (admin role).
+ *       422:
+ *         description: Validation error. Check the request body for missing or invalid fields.
+ *       500:
+ *         description: Internal server error.
+ *     deprecated: false
+ */
+
 router.post(
   '/cryptos',
   isAuth,
@@ -79,7 +181,39 @@ router.post(
   cryptoController.addCrypto
 );
 
-// DELETE pour supprimer une crypto-monnaie (seulement admin)
+
+/**
+ * @swagger
+ * /cryptos/{cmid}:
+ *   delete:
+ *     summary: Delete a Cryptocurrency
+ *     description: Deletes a cryptocurrency based on its ID.
+ *     tags:
+ *       - Cryptos
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - name: cmid
+ *         in: path
+ *         description: Cryptocurrency ID.
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       204:
+ *         description: Cryptocurrency deleted successfully.
+ *       401:
+ *         description: User is not authenticated.
+ *       403:
+ *         description: User does not have the necessary permissions (admin role).
+ *       404:
+ *         description: The specified cryptocurrency was not found.
+ *       500:
+ *         description: Internal server error.
+ *     deprecated: false
+ */
+
+
 router.delete('/cryptos/:cmid', isAuth, isAdmin, cryptoController.deleteCrypto);
 
 export default router;
