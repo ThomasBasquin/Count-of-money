af263668a4d3224fdb1c64bc0fd1addb779f98b5
Thomas <thomas.basquin2@gmail.com>
2023-11-27 14:55:48 +0100

    Add role field to user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -45,6 +45,11 @@ const userSchema = new mongoose.Schema(
         trim: true,
       },
     ],
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user',
+    },
   },
   { timestamps: true }
 );
