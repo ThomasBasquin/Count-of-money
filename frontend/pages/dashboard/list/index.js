89ece140844be0e496efee91aead773462704d3d
Mathéo Vitali <matheo.vitali@epitech.eu>
2023-12-19 10:17:04 +0100

    (change) small changes to the list of cryptos

--- frontend/pages/dashboard/list/index.js
+++ frontend/pages/dashboard/list/index.js
@@ -18,7 +18,7 @@ const HomePage = () => {
         for (let i = 0; i != tabToSearchIn.length; i++) {
             tabToFill.push(
             <div className=" px-3 py-1 m-2 rounded-full bg-gradient-to-r from-gray-700 via-gray-900 to-black border border-gray-500">
-                <div className='grid grid-cols-6'>
+                <div className='grid grid-cols-7'>
                     <div className='grid grid-cols-2 my-auto'>
                         <div className='mx-3 w-fit m-auto'>
                             <Image src={tabToSearchIn[i].imageUrl} alt={tabToSearchIn[i].name} width={75} height={75} />
@@ -29,16 +29,16 @@ const HomePage = () => {
                             <h1>{tabToSearchIn[i].currentPrice.toFixed(2)} €</h1>
                         </div>
                     </div>
-                    <div className='flex flex-row'>
-                        <h1 className='m-auto text-white text-lg'>Today: </h1>
-                        <div className='flex flex-col text-white font-bold m-auto'>
+                    <div className='m-auto w-3/4 flex flex-col border-x-2 rounded-lg border-gray-600 my-[-4px] shadow-lg shadow-gray-600'>
+                        <h1 className='m-auto text-white'>Today: </h1>
+                        <div className='flex flex-col text-white font-bold m-auto mb-2'>
                             <h1 className='text-green-500'>{tabToSearchIn[i].highestPriceOfDay} € ↑</h1>
-                            <h1 className='mx-auto'>|</h1>
+                            <h1 className='mx-auto my-[-6px]'>-</h1>
                             <h1 className='text-red-500'>{tabToSearchIn[i].lowestPriceOfDay} € ↓</h1>
                         </div>
                     </div>
-                    <div className='grid grid-rows-2 w-1/2'>
-                        <div className='ml-5 underline underline-offset-2 text-white'>
+                    <div className='grid grid-rows-2 m-auto'>
+                        <div className='text-white'>
                             <h1>Last Hour :</h1>
                         </div>
                         <div className='m-auto'>
@@ -63,8 +63,8 @@ const HomePage = () => {
                             }
                         </div>
                     </div>
-                    <div className='grid grid-rows-2 w-1/2'>
-                        <div className='ml-5 underline underline-offset-2 text-white'>
+                    <div className='grid grid-rows-2 m-auto'>
+                        <div className='text-white'>
                             <h1>Last Day :</h1>
                         </div>
                         <div className='m-auto'>
@@ -89,8 +89,8 @@ const HomePage = () => {
                             }
                         </div>
                     </div>
-                    <div className='grid grid-rows-2 w-1/2'>
-                        <div className='ml-5 underline underline-offset-2 text-white'>
+                    <div className='grid grid-rows-2 m-auto'>
+                        <div className='text-white'>
                             <h1>Last 7 Days :</h1>
                         </div>
                         <div className='m-auto'>
@@ -115,8 +115,8 @@ const HomePage = () => {
                             }
                         </div>
                     </div>
-                    <div className='grid grid-rows-2 w-1/2'>
-                        <div className='ml-5 underline underline-offset-2 text-white'>
+                    <div className='grid grid-rows-2 m-auto'>
+                        <div className='text-white'>
                             <h1>Last Month :</h1>
                         </div>
                         <div className='m-auto'>
@@ -140,6 +140,10 @@ const HomePage = () => {
                                 )
                             }
                         </div>
+                    </div>
+                    <div className='mx-auto border-r-[25px] border-b-[70px]
+border-l-[25px] border-solid border-r-transparent
+border-l-transparent border-b-yellow-500 border-l-red-500'>
                     </div>
                 </div>
             </div>
